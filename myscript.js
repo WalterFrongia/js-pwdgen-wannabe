041f4034aaabfc81b28a5a5ec2bb15fbc7198cb7
WalterFrongia <walter.frongia@libero.it>
2021-08-04 16:55:24 +0200

    Aggiunta di tutte le variabili e completamento progetto

--- myscript.js
+++ myscript.js
@@ -2,3 +2,14 @@
 var nome = prompt("inserisci qui il tuo nome")
 var cognome = prompt("inserisci qui il tuo cognome")
 var colorePreferito = prompt("inserisci qui il tuo colore preferito")
+
+document.getElementById("ins-nome").innerHTML = nome
+
+document.getElementById("ins-cognome").innerHTML = cognome
+
+document.getElementById("ins-colore").innerHTML = colorePreferito
+
+var password = nome + cognome + colorePreferito
+
+document.getElementById("ins-password").innerHTML = nome + cognome + colorePreferito
+
